41288b03d5c3c5c260c19fceef3abf82b9c32faa
keith-fernandes <72031382+keith-fernandes@users.noreply.github.com>
2023-04-30 18:53:09 +0100

    adding files

---
+++ registration-login2/src/main/java/net/javaguides/registrationlogin2/RegistrationLogin2Application.java
@@ -0,0 +1,13 @@
+package net.javaguides.registrationlogin2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLogin2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLogin2Application.class, args);
+	}
+
+}
---
+++ registration-login2/src/test/java/net/javaguides/registrationlogin2/RegistrationLogin2ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.registrationlogin2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLogin2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
